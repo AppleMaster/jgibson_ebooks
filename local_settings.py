1b82136a8c33b6adf1942ca7a45854312926f475
stellasoft-jim <jim@stellasoft.co.uk>
2016-03-06 13:33:37 +0000

    Fix settings again...

--- local_settings.py
+++ local_settings.py
@@ -3,15 +3,15 @@ Local Settings for a heroku_ebooks account. #fill in the name of the account you
 '''
 
 #configuration
-MY_CONSUMER_KEY = 'Your Twitter API Consumer Key'
-MY_CONSUMER_SECRET = 'Your Consumer Secret Key'
-MY_ACCESS_TOKEN_KEY = 'Your Twitter API Access Token Key'
-MY_ACCESS_TOKEN_SECRET = 'Your Access Token Secret'
+MY_CONSUMER_KEY = 'cH6NttiAoLbE7IL9SCmuMPtb8'
+MY_CONSUMER_SECRET = 'xGPca4C5CwlDdcHrCLIAyz86pYFdvBn4dScineb1jQKIHegMHW'
+MY_ACCESS_TOKEN_KEY = '706461719594475520-t0sWT6KuGzqnIqnBUMj8hVZRm4CSXmB'
+MY_ACCESS_TOKEN_SECRET = '	8UBipyh4B3pvFRp453HSL13lZMD4JN6hIG56hWOeBLdN4'
 
-SOURCE_ACCOUNTS = [""] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
+SOURCE_ACCOUNTS = ["J_Gibsonn, DziekDB"] #A list of comma-separated, quote-enclosed Twitter handles of account that you'll generate tweets based on. It should look like ["account1", "account2"]. If you want just one account, no comma needed.
 ODDS = 8 #How often do you want this to run? 1/8 times?
 ORDER = 2 #how closely do you want this to hew to sensical? 1 is low and 3 is high.
-DEBUG = True #Set this to False to start Tweeting live
+DEBUG = False #Set this to False to start Tweeting live
 STATIC_TEST = False #Set this to True if you want to test Markov generation from a static file instead of the API.
-TEST_SOURCE = ".txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
-TWEET_ACCOUNT = "" #The name of the account you're tweeting to.
+TEST_SOURCE = "tweets.txt" #The name of a text file of a string-ified list for testing. To avoid unnecessarily hitting Twitter API.
+TWEET_ACCOUNT = "jgibson_ebooks" #The name of the account you're tweeting to.
